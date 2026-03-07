9613028b2babededfb1d3acb70cecdc5287ce44d
ania <mysubscriptionsolution@gmail.com>
2026-03-07 17:23:02 +0100

    api tests

---
+++ SiiHackathon/Helpers/JsonHelper.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiiHackathon.Helpers
+{
+    internal class JsonHelper
+    {
+        public static string SerializeToJson(JsonSerializer serializer, object payload)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            using (var stringWriter = new StringWriter(stringBuilder))
+            {
+                serializer.Serialize(stringWriter, payload);
+            }
+            var jsonStr = stringBuilder.ToString();
+            return jsonStr;
+        }
+    }
+}
--- SiiHackathon/Tests/ApiTests.cs
+++ SiiHackathon/Tests/ApiTests.cs
@@ -1,15 +1,53 @@
-﻿namespace SiiHackathon.Tests
+﻿using Newtonsoft.Json;
+using SiiHackathon.Models.Requests;
+using System.Collections;
+using System.IO;
+using System.Net;
+using System.Text.Json;
+
+namespace SiiHackathon.Tests
 {
     public class ApiTests : BaseApiTest
     {
-        [Test]
-        public async Task GetProducts_ReturnOkStatus()
+        [TestCase(HttpStatusCode.Created)]
+        public async Task CreateProduct_WithValidData_ReturnCreated(HttpStatusCode expectedStatusCode)
         {
+            //Arrange
+            var names = new List<Dictionary<string, string>>
+                {
+                    new Dictionary<string, string>
+                    {
+                        { "en", "Test Product" },
+                        { "pl", "Produkt Testowy" }
+                    }
+                };
+            var type = "TestProduct";
+            var requestBody = new Product(type, names);
+           // var requestBodyJson = JsonConvert.SerializeObject(requestBody);
 
-            var response = await Request.GetAsync(Const.ApiUrls.GetProducts);
-            var responseBody = await response.TextAsync();
-             Console.WriteLine(responseBody);
-             Assert.That(response.Status, Is.EqualTo(200), "API should return 200 OK");
+            //Act
+            await Request.PostAsync(Const.ApiUrls.Products, new() { DataObject =  requestBody});
+                var response = await Request.PostAsync(Const.ApiUrls.Products);
+                var responseBody = await response.TextAsync();
+                Console.WriteLine(responseBody);
+
+            //Assert
+            Assert.That(response.Status, Is.EqualTo((int)expectedStatusCode), "API should return 201 Created");
+        }
+
+        [TestCase(HttpStatusCode.Created)]
+        public async Task GetProduct_ReturnOk(HttpStatusCode expectedStatusCode)
+        {
+            //Arrange
+           
+            //Act
+            
+            var response = await Request.GetAsync(Const.ApiUrls.Products);
+
+            Console.WriteLine(response);
+
+            //Assert
+            Assert.That(response.Status, Is.EqualTo((int)expectedStatusCode), "API should return 201 Created");
         }
     }
 }

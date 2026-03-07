fedc84300dbf2578e6df8eb0a97886b21d889b95
Marcin Szymczyk <szmynczyk+narzedzia@gmail.com>
2026-03-07 11:58:12 +0100

    Code cleanup

--- SiiHackathon/Tests/BasicTests.cs
+++ SiiHackathon/Tests/BasicTests.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Playwright;
+﻿using SiiHackathon.Pages;
 
 namespace SiiHackathon.Tests
 {
@@ -32,12 +32,7 @@ namespace SiiHackathon.Tests
             var firstName = Helpers.GenerateTestData.GenerateRandomName();
             var lastName = Helpers.GenerateTestData.GenerateRandomName();
             var registrationPage = new Pages.RegistrationPage(_page);
-            await registrationPage.FillInRegisterForm("Test", "Test", ");
-
-
-             private ILocator FirstNameInput => _page.Locator("#field-firstname");
-        private ILocator LastNameInput => _page.Locator("#field-lastname");
-        private ILocator EmailInput => _page.Locator("#field-email");
-        private ILocator PasswordInput => _page.Locator("#field-password");
+            //await registrationPage.FillInRegisterForm("Test", "Test", ");
+        }
     }
 }

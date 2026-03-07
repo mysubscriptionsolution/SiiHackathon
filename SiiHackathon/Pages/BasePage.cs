b2475d3a6407743d9e8705397a16cf0943ed841c
ania <mysubscriptionsolution@gmail.com>
2026-03-07 12:34:54 +0100

    registration test updates

--- SiiHackathon/Pages/BasePage.cs
+++ SiiHackathon/Pages/BasePage.cs
@@ -5,10 +5,5 @@ namespace SiiHackathon.Pages
     internal class BasePage(IPage page)
     {
         protected readonly IPage _page = page;
-
-        public void GoToUrl(string url)
-        {
-            _page.GotoAsync(url);
-        }
     }
 }

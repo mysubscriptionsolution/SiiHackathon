0c880b9ccdd4a1a07cf47acf83c889ff31e34d20
ania <mysubscriptionsolution@gmail.com>
2026-03-07 11:07:26 +0100

    register page changes

--- SiiHackathon/Pages/RegistrationPage.cs
+++ SiiHackathon/Pages/RegistrationPage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Playwright;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace SiiHackathon.Pages
 {
-    internal class RegistrationPage
+    internal class RegistrationPage 
     {
+        private ILocator FirstNameInput => page.GetById("field-firstname");
+        private ILocator LastNameInput => page.GetById("field-lastname");
+        private ILocator EmailInput => page.GetById("field-email");
+        private ILocator PasswordInput => page.GetById("field-password");
+        private ILocator AgreeTermsCheckboxt => page.Locator("#field-email");
+
     }
 }

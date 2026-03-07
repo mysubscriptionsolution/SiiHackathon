49d6d534d3ae51b839ed78ad02b43445a04cd984
ania <mysubscriptionsolution@gmail.com>
2026-03-07 11:25:49 +0100

    cleaning

--- SiiHackathon/Tests/BasicTests.cs
+++ SiiHackathon/Tests/BasicTests.cs
@@ -1,4 +1,6 @@
-﻿namespace SiiHackathon.Tests
+﻿using Microsoft.Playwright;
+
+namespace SiiHackathon.Tests
 {
     internal class BasicTests : BaseTest
     {
@@ -8,5 +10,20 @@
             var loginPage = new Pages.LoginPage(_page);
             await loginPage.Login("admin@local.dev", "hackathon-sii-2026");
         }
+
+        [Test]
+        public async Task RegisterAccount()
+        {
+            var email = Helpers.GenerateTestData.GenerateRandomEmail();
+            var firstName = Helpers.GenerateTestData.GenerateRandomName();
+            var lastName = Helpers.GenerateTestData.GenerateRandomName();
+            var registrationPage = new Pages.RegistrationPage(_page);
+            await registrationPage.FillInRegisterForm("Test", "Test", ");
+
+
+             private ILocator FirstNameInput => _page.Locator("#field-firstname");
+        private ILocator LastNameInput => _page.Locator("#field-lastname");
+        private ILocator EmailInput => _page.Locator("#field-email");
+        private ILocator PasswordInput => _page.Locator("#field-password");
     }
 }
